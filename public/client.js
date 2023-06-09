12e2dfb0617c403b4199535a86052a7a9d8f4f41
RamilSC <ramil.cobillajr@tup.edu.ph>
2023-06-09 11:28:42 +0800

    Update client.js

--- public/client.js
+++ public/client.js
@@ -37,7 +37,7 @@ fetch("/ws-url")
       .then((data) => {
         console.log(data);
         // Start the video player
-        player = new JSMpeg.Player(`ws://${location.hostname}:${wsUrl}/`, {
+        player = new JSMpeg.Player(`wss://${location.hostname}:${wsUrl}/`, {
           canvas: document.getElementById("videoCanvas"),
           autoplay: true,
         });

5a79dab82ec46a3645dc3870ac8984436e4c84ba
RamilSC <ramil.cobillajr@tup.edu.ph>
2023-06-09 11:23:26 +0800

    Added ffmpeg static

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000; // Use the PORT environment variable if it exists
+const port = process.env.PORT || 3000;
 
 const Stream = require("node-rtsp-stream");
+const ffmpegStatic = require("ffmpeg-static");
 
 let stream;
 
-// This is needed to parse the body of POST requests
 app.use(express.json());
+
+app.get("/ws-url", (req, res) => {
+  res.send({ wsUrl: process.env.WS_URL || "6789" });
+});
 
 app.post("/set-rtsp", (req, res) => {
   const rtspUrl = req.body.rtspUrl;
 
-  // Check if a stream is already running and stop it if it is
   if (stream) {
     stream.stop();
   }
 
-  // Start a new stream with the new URL
   stream = new Stream({
     name: "name",
     streamUrl: rtspUrl,
-    wsPort: 9999,
+    wsPort: 6789,
+    ffmpegPath: ffmpegStatic.path, // Add this line
     ffmpegOptions: {
+      "-f": "mpegts",
+      "-codec:v": "mpeg1video",
+      "-b:v": "1000k",
       "-stats": "",
-      "-r": 30,
+      "-r": 25,
+      "-s": "640x480",
+      "-bf": 0,
+      "-codec:a": "mp2",
+      "-ar": 44100,
+      "-ac": 1,
+      "-b:a": "128k",
     },
   });
 
